ca8139eaa0376f0c63bbb9bf02727ccdf8d81041
Matan <matan3sh@gmail.com>
2024-02-06 19:29:35 +0200

    Implement Base Styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/app/components/Navbar"
 import Provider from "@/app/utils/Provider"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Fira_Code } from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const firaCode = Fira_Code({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,10 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${firaCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}
+      >
         <Provider>
           <Navbar />
-          <main className="mx-auto max-w-5xl px-6">{children}</main>
+          <main className="h-full mx-auto max-w-5xl px-6">{children}</main>
         </Provider>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // or 'media' or 'class
   theme: {},
   plugins: [require("@tailwindcss/typography")],
 }
