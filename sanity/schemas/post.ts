139a18cf920b32d8cfb58fc625c2f2be775468db
Matan <matan3sh@gmail.com>
2024-02-09 15:40:24 +0200

    Finish Post Schema

--- sanity/schemas/post.ts
+++ sanity/schemas/post.ts
@@ -1,3 +1,5 @@
+import { Rule } from "sanity"
+
 export const post = {
   name: "post",
   title: "Post",
@@ -8,6 +10,38 @@ export const post = {
       name: "title",
       title: "Title",
       type: "string",
+      validation: (rule: Rule) => rule.required().error("Required"),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+      validation: (rule: Rule) => rule.required().error("Required"),
+    },
+    {
+      name: "publishedAt",
+      title: "Published At",
+      type: "datetime",
+      initialValue: () => new Date().toISOString(),
+    },
+    {
+      name: "excerpt",
+      title: "Excerpt",
+      type: "text",
+      validation: (rule: Rule) => rule.max(200).error("Max 200 characters"),
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "array",
+      of: [
+        { type: "block" },
+        {
+          type: "image",
+          fields: [{ type: "text", name: "alt", title: "Alt" }],
+        },
+      ],
     },
   ],
 }
